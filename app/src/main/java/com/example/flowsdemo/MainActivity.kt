2cde29496cbddcb2de28ae6228c26c07be346380
tejaskhartude <tejaskhartude@gmail.com>
2022-05-30 15:27:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/flowsdemo/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.flowsdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.flowsdemo.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var flow: Flow<Int>
+    val TAG:String = "Main"
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       // setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupFlows()
+        setupClicks()
+
+    }
+
+    private fun setupClicks() {
+        binding.btnLaunch.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch{
+                flow.collect {
+                    Log.d(TAG,it.toString())
+                }
+            }
+        }
+
+    }
+
+    private fun setupFlows() {
+        flow = flow {
+            Log.d(TAG, "Start Flow")
+            (0..10).forEach {
+                // emit data after every 500 milliseconds
+                delay(500)
+                Log.d(TAG,"Emitting $it")
+                emit(it)
+            }
+        }.flowOn(Dispatchers.Default)
+
+    }
+}
